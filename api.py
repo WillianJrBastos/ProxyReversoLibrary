d24a341240d017131a91968b44486803dd8d07a0
WillianJrBastos <wjbastos1@gmail.com>
2026-09-06 12:08:54 -0300

    Configuração do banco PostgreSQL (função para abrir conexao com o banco)

--- api.py
+++ api.py
@@ -3,3 +3,14 @@ import psycopg2
 import psycopg2.extras
 
 app = Flask(__name__)
+
+DB_CONFIG = {
+    "host": "192.168.56.30",
+    "dbname": "biblioteca",
+    "user": "root",
+    "password": "root123#",
+    "port": 5432
+}
+
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
